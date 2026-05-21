d6e9de06d689117da4f83106f08e85cceb6fae4b
GAMANASREE8985 <Suryadp042@gmail.com>
2026-05-22 01:48:14 +0530

    fixed frontend structure

--- frontend/Backend/db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-# DATABASE CONNECTION
-def get_db():
-
-    conn = sqlite3.connect("schedule.db")
-
-    conn.row_factory = sqlite3.Row
-
-    return conn
--- frontend/Tools/PySpark/spark_setup.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql import SparkSession
-
-def get_spark():
-    return SparkSession.builder \
-        .appName("Internship_Project") \
-        .master("local[*]") \
-        .getOrCreate()
